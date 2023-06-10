b0084dd34dea92570e53f876ed8d5579221b8264
Emanoel Carmo <carmoemanoel@gmail.com>
2023-06-10 19:15:04 -0300

    Corrigindo tipos primitivos e ordem de precedencia

--- Exercicios/exer_02.py
+++ Exercicios/exer_02.py
@@ -3,6 +3,6 @@
 numero = int(input('Digite um número: '))
 dobro = numero * 2
 triplo = numero * 3
-sqrt = numero ** 2
+sqrt = numero ** (1/2)
 
 print('O numero digitado foi {} e o seu dobro é {}, seu triplo {} e a sua raiz quadrada é {}.'.format(numero, dobro, triplo, sqrt))
--- Exercicios/exer_03.py
+++ Exercicios/exer_03.py
@@ -1,7 +1,7 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
 
-nota1 = int(input('Digite a sua primeira nota: '))
-nota2 = int(input('Digite a sua segunda nota: '))
+nota1 = float(input('Digite a sua primeira nota: '))
+nota2 = float(input('Digite a sua segunda nota: '))
 
 media = (nota1 + nota2) / 2
 
