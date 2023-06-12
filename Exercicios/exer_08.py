110c8a1926086778653902d2a3529e6fdc1cb674
Emanoel Carmo <carmoemanoel@gmail.com>
2023-06-12 16:12:58 -0300

    Fazendo exercicios

--- Exercicios/exer_08.py
+++ Exercicios/exer_08.py
@@ -1,1 +1,12 @@
-# Faça um algoritmo que leia l o preço de um produto e mostre o seu novo preço com um desconto de 5%.
+# Faça um algoritmo que leia o preço de um produto e mostre o seu novo preço com um desconto de 5%.
+
+
+produto = float(input('Digite o preço do produto: '))
+
+porcentagem  = 5/100
+
+des =  produto * porcentagem
+
+total = produto - des
+
+print('O valor do produto é {} e com o desconto de 5% passa a ser {}'.format(produto, total))
--- Exercicios/exer_09.py
+++ Exercicios/exer_09.py
@@ -1,2 +1,11 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre o seu novo salário com 15% de aumento;
 
+salario = float(input('Digite o preço do Salário: '))
+
+porcentagem  = 15/100
+
+des =  salario * porcentagem
+
+reajuste = salario + des
+
+print('O salário atual é {} e teve um reajuste de 15% passou a ser {}'.format(salario, reajuste))
