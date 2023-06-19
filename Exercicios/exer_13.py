c1eb1ec99f39f5a8c1129624995fb4a95c4d44ef
Emanoel Carmo <carmoemanoel@gmail.com>
2023-06-19 20:36:04 -0300

    Inserindo correção dos exercios

--- Exercicios/exer_13.py
+++ Exercicios/exer_13.py
@@ -1,2 +1,8 @@
 # Um professor quer sortear um dos seus quatros alunos para apagar o quadro.
 # Faça um programa que ajude ele lendo o nome deles e escrevendo o nome escolhido.
+
+import random 
+
+alunos = ['João', 'Paulo', 'Alice', 'Nathalia']
+
+print(random.choice(alunos))
--- Exercicios/exer_14.py
+++ Exercicios/exer_14.py
@@ -1,2 +1,8 @@
 # O mesmo professor do desafio anterior quer sortear a ordem de apresentação do trabalho dos alunos.
 # Faça um programa que leia o nome dos quatros alunos e mostre a ordem sorteada.
+
+import random
+
+sorteio = ['Emanoel' , 'Alexandra' , 'Sthella' , 'Merlyn']
+
+print(random.choice(sorteio))
