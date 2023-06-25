432ceaab75749b44e79f1fc99fe7ac958fbd6bed
Emanoel Carmo <carmoemanoel@gmail.com>
2023-06-25 18:04:51 -0300

    Fazendo exercicio mp3

--- Exercicios/exer_15.py
+++ Exercicios/exer_15.py
@@ -1,1 +1,8 @@
-# Faça um programa em Python que abra e reproduza o número de um arquivo em MP3
+# Faça um programa em Python que abra e reproduza uma música de um arquivo em MP3
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('exer_15.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+
