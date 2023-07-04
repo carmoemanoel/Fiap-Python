97eca05946b543db477970f3149f2377761f5235
Emanoel Carmo <carmoemanoel@gmail.com>
2023-07-04 18:30:24 -0300

    Resolução do primeiro exercicio

--- Exercicios/exer_17.py
+++ Exercicios/exer_17.py
@@ -5,3 +5,13 @@
 * Quantas ao todo o (sem considerar espaços);
 * Quantas letras tem o primero nome
 '''
+
+nome = input('Digite o seu nome completo em letras minisculas:')
+print('O nome com todas as letras em maiusculas eh', nome.upper())
+nome = input('Digite o seu nome completo em letras maiusculas:')
+print('O nome com todas as letras minisculas eh',nome.lower())
+nome = input('Digite o seu nome completo para saber o total de letras:')
+print('O total de letras desse nome eh', len(nome.strip()))
+nome = input('Digite o seu nome completo:')
+dividido = nome.split()
+print('A quantidade de letras do primero nome eh',len(dividido[0].strip()))
